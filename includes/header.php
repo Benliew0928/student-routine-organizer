65c6198f815a7a6b8d6dcb73334ba5cf40edc67d
juinhong05 <juinhong05@gmail.com>
2026-07-29 00:44:19 +0800

    exercise ui second update

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@ $pageTitle = $pageTitle ?? APP_NAME;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= escapeOutput($pageTitle); ?> | <?= APP_NAME; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>/assets/css/style.css?v=20260725-exercise-blog-x-complete">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>/assets/css/style.css?v=20260728-exercise-achievement-roman">
 </head>
 <body>
     <?php require __DIR__ . '/navbar.php'; ?>
